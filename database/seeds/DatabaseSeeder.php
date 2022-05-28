b545a9cb1b382d9b9069c8f2eeca634ae9ab2149
Rafly Rizky <raflyrizkyinriawan@gmail.com>
2022-05-28 12:40:59 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $users = [
+            [
+               'name'=>'Admin User',
+               'email'=>'admin@email.com',
+               'type'=>1,
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'Manager User',
+               'email'=>'manager@email.com',
+               'type'=> 2,
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'User',
+               'email'=>'user@email.com',
+               'type'=>0,
+               'password'=> bcrypt('123456'),
+            ],
+        ];
+    
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
     }
 }
